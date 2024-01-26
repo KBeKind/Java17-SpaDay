89018c522d52b0a57eb702a2ccb95f46205f756d
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-01-25 19:08:15 -0600

    commit

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,26 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "username cant be blank")
+    @Size(min=5, max = 15, message = "username must be between 5 and 15 char")
     private String username;
+
+    @Email
     private String email;
+
+    @Size(min=6, message = "password must be at least 6 char")
+    @NotBlank(message = "password cant be blank")
     private String password;
+
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
+
 
     public User() {
 
@@ -38,6 +55,28 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+
+    }
+
+    private void checkPassword(){
+        if (this.password != null && this.verifyPassword != null){
+            if(this.password.equals(this.verifyPassword)){
+                //Passwords good
+            } else {
+                this.verifyPassword = null;
+            }
+        }
+    }
+
 }
 
